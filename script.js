45f7bc1bb848a73aa550eaac1d998b5e4fa66464
wathfield <wathfield@users.noreply.github.com>
2024-01-24 20:18:54 +0100

    adding background input and transition

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let intervalId;
 
     function startStroboscope() {
+        // Background color
+        const bodyColor = document.getElementById('backColorInput').value;
+
+        //Main inputs
         const interval = parseFloat(document.getElementById('intervalInput').value) * 1000;
         const length = parseFloat(document.getElementById('lengthInput').value) * 1000;
         const color = document.getElementById('colorInput').value;
+        const strobeContainer = document.getElementById('strobeContainer');
+        const colorTransition = parseFloat(document.getElementById('TransitionTimeInput').value);
+        strobeContainer.style.transition = `background-color ${colorTransition}s`;
         intervalId = setInterval(() => {
-            const strobeContainer = document.getElementById('strobeContainer');
             strobeContainer.style.backgroundColor = color;
             setTimeout(() => {
-                strobeContainer.style.backgroundColor = 'black';
+                strobeContainer.style.backgroundColor = bodyColor;
             }, length);
         }, interval);
     }
@@ -16,3 +22,10 @@ let intervalId;
         clearInterval(intervalId);
         document.getElementById('strobeContainer').style.backgroundColor = 'black';
     }
+
+
+    // Add another input to choose a transition and make time adaptable
+    // make site phone adaptable
+    // show and hide Control settings.
+
+
